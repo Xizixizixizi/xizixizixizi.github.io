80db3c017fb20c60797b2143e8cde7ba85e01d82
Xizixizixizi <58480657+Xizixizixizi@users.noreply.github.com>
2020-05-29 17:30:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+//https://stackoverflow.com/questions/32540044/html-display-current-date/32540196, Lance
